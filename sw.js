ce07959cdcd26030b79932c45278b840a68c7364
snake3210 <148596277+snake3210@users.noreply.github.com>
2026-08-09 11:58:19 +0800

    Make mobile launcher installable PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'line-work-brain-launcher-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/line-work-brain-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
